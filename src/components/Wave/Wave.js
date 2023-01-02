5370f9c5a0fde44352af7d5c63ba0f29a2943672
Rachel Fang <yanhanfan828@gmail.com>
2023-01-03 00:20:55 +1100

    feat: build frontend structure

---
+++ src/components/Wave/Wave.js
@@ -0,0 +1,9 @@
+import './Wave.css'
+
+const Wave = () => {
+    return (
+        <img src='http://weaths.kitmanyiu.com/static/media/wave-bot.6cd18bc0.png' className='wave' alt='dec'/>
+    )
+}
+
+export default Wave;
